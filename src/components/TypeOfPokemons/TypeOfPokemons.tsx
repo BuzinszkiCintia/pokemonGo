c97fd91dc3f515a21544f3099cf7af17fa6022a3
BuzinszkiCintia <buzinszki.cintia@gmail.com>
2023-01-20 09:34:46 +0100

    added Loader and changed Fetching data

--- src/components/TypeOfPokemons/TypeOfPokemons.tsx
+++ src/components/TypeOfPokemons/TypeOfPokemons.tsx
@@ -1,6 +1,7 @@
 import Fetch from "../Fetch/Fetch";
-import { TypeOfPokemonsContainer } from "./TypeOfPokemons.style";
 import TypeOfPokemonCard from "./TypeOfPokemonCard";
+import { TypeOfPokemonsContainer } from "./TypeOfPokemons.style";
+import { Loader } from "../Pokemon/Pokemon.style";
 
 interface Props {
   url: string;
@@ -17,53 +18,51 @@ interface Pokemons {
 
 const TypeOfPokemons = ({ url, checked, search }: Props) => {
   let catchedPokemons = JSON.parse(sessionStorage.getItem("Catched") || "[]");
+  const { data, loading } = Fetch(url);
 
-  let PokemonTypes = sessionStorage.getItem("PokemonTypes");
-  let data;
-  console.log("Pokemontypes", PokemonTypes);
-  if (PokemonTypes === null || PokemonTypes === "undefined") {
-    sessionStorage.setItem("PokemonTypes", JSON.stringify(Fetch(url).data));
-  } else {
-    data = JSON.parse(PokemonTypes);
-  }
-  console.log("data", data);
   return (
-    <TypeOfPokemonsContainer>
-      {data?.pokemon
-        .filter((f: any) => {
-          if (checked.length > 0 && checked.includes(f.pokemon.name)) {
-            return f.pokemon;
-          }
-          if (search.length > 0 && f.pokemon.name.includes(search)) {
-            return f.pokemon;
-          }
-          if (checked.length <= 0 && search.length <= 0) {
-            return f.pokemon;
-          }
-        })
-        .map((pokemon: Pokemons, index: number) => {
-          let poke = pokemon.pokemon;
-          if (catchedPokemons.includes(poke.name)) {
-            return (
-              <TypeOfPokemonCard
-                key={index}
-                poke={poke}
-                index={index}
-                border={true}
-              />
-            );
-          } else {
-            return (
-              <TypeOfPokemonCard
-                key={index}
-                poke={poke}
-                index={index}
-                border={false}
-              />
-            );
-          }
-        })}
-    </TypeOfPokemonsContainer>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <TypeOfPokemonsContainer>
+          {data?.pokemon
+            .filter((f: any) => {
+              if (checked.length > 0 && checked.includes(f.pokemon.name)) {
+                return f.pokemon;
+              }
+              if (search.length > 0 && f.pokemon.name.includes(search)) {
+                return f.pokemon;
+              }
+              if (checked.length <= 0 && search.length <= 0) {
+                return f.pokemon;
+              }
+            })
+            .map((pokemon: Pokemons, index: number) => {
+              let poke = pokemon.pokemon;
+              if (catchedPokemons.includes(poke.name)) {
+                return (
+                  <TypeOfPokemonCard
+                    key={index}
+                    poke={poke}
+                    index={index}
+                    border={true}
+                  />
+                );
+              } else {
+                return (
+                  <TypeOfPokemonCard
+                    key={index}
+                    poke={poke}
+                    index={index}
+                    border={false}
+                  />
+                );
+              }
+            })}
+        </TypeOfPokemonsContainer>
+      )}
+    </>
   );
 };
 
